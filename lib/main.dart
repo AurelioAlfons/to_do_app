f06349d3b52d9b6de03eb8f0d8e369b3fd18dec7
AurelioAlfons <yuroalfons0407@gmail.com>
2024-09-25 13:04:27 +1000

    Ver 1.2: Add the Appbar

--- lib/main.dart
+++ lib/main.dart
@@ -16,13 +16,43 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        backgroundColor: Colors.orange,
+        backgroundColor: const Color.fromARGB(255, 96, 92, 191),
+        appBar: AppBar(
+          backgroundColor: const Color.fromARGB(255, 108, 4, 255),
+          shadowColor: Colors.black,
+          elevation: 10,
+          // Put an icon on the left (leading)
+          leading: const Icon(
+            Icons.menu,
+            color: Colors.white,
+            size: 28,
+          ),
+          // Put an icon on the left (actions)
+          actions: [
+            IconButton(
+                onPressed: () {},
+                icon: const Icon(
+                  Icons.logout,
+                  color: Colors.white,
+                  size: 28,
+                ))
+          ],
+          title: const Center(
+            child: Text(
+              'Aurelio Todo',
+              style: TextStyle(
+                  fontSize: 26,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold),
+            ),
+          ),
+        ),
         body: Center(
           child: Container(
             width: 300,
             height: 300,
             decoration: BoxDecoration(
-                color: const Color.fromARGB(255, 238, 14, 48),
+                color: const Color.fromARGB(255, 233, 11, 56),
                 borderRadius: BorderRadius.circular(20)),
             padding: const EdgeInsets.all(25),
             child: const Column(
