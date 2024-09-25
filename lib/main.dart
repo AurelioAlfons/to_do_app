3a996c5acbf9cd62199851f73ca6715919459db6
AurelioAlfons <yuroalfons0407@gmail.com>
2024-09-25 12:47:46 +1000

    Ver 1.1: Center Container Body, Text & Icon

--- lib/main.dart
+++ lib/main.dart
@@ -13,9 +13,45 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(),
+      home: Scaffold(
+        backgroundColor: Colors.orange,
+        body: Center(
+          child: Container(
+            width: 300,
+            height: 300,
+            decoration: BoxDecoration(
+                color: const Color.fromARGB(255, 238, 14, 48),
+                borderRadius: BorderRadius.circular(20)),
+            padding: const EdgeInsets.all(25),
+            child: const Column(
+              children: [
+                Center(
+                  child: Text(
+                    'Hello World',
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 28,
+                        fontWeight: FontWeight.bold),
+                  ),
+                ),
+                // Use SizedBox to add a space
+                SizedBox(
+                  height: 60,
+                ),
+                Center(
+                  child: Icon(
+                    Icons.favorite,
+                    color: Colors.white,
+                    size: 80,
+                  ),
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
