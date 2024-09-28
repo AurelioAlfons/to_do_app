f83a326d5cd4cc5bf092b33807dc5749eb862fe9
AurelioAlfons <yuroalfons0407@gmail.com>
2024-09-28 17:59:40 +1000

    Ver 2.7.1: Add ssh pass not important

---
+++ ssh_pass.sh
@@ -0,0 +1,9 @@
+ssh-keygen -t ed25519 -b 4096 -C "yuroalfons0407@gmail.com" -f bitbucket
+
+ssh-add ~/bitbucket
+
+ssh key name: bitbucket
+
+ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJKE5UWYN1UOgLTGsKtGnA0Wfh8LyKfwpDE+IeglcmHY yuroalfons0407@gmail.com
+
+Keypass: pass123
