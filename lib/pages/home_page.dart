5622c7e2307c870284581eb070b1c3d9237bf1e0
AurelioAlfons <yuroalfons0407@gmail.com>
2024-10-02 17:37:52 +1000

    Ver 2.10: Move home content to another file

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,9 +1,9 @@
 // home.dart
 import 'package:flutter/material.dart';
 import 'package:to_do_app/pages/explore_page.dart';
+import 'package:to_do_app/pages/home_content.dart'; // Import the HomeContent widget
 import 'package:to_do_app/widget/navigation.dart';
 
-// Create class HomePage
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
@@ -16,33 +16,25 @@ class _HomePageState extends State<HomePage> {
   int _currentIndex = 0;
 
   // Define the list of pages
-  // ignore: unused_field
   final List<Widget> _pages = [
-    const HomePage(),
+    const HomeContent(), // Use HomeContent from its own file
     const ExplorePage(),
   ];
 
   void _onTap(int index) {
-    // Correct the setState syntax
+    // Update the current index when a bottom nav item is tapped
     setState(() {
       _currentIndex = index;
-      // Add any navigation logic here
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // Scaffold
     return Scaffold(
-      // Bg Color
-      backgroundColor: const Color.fromARGB(255, 96, 92, 191),
-
-      // AppBar
       appBar: AppBar(
         backgroundColor: const Color(0xFF6C04FF),
         shadowColor: Colors.black,
         elevation: 10,
-        // Left Button
         leading: IconButton(
           onPressed: () {},
           icon: const Icon(
@@ -51,7 +43,6 @@ class _HomePageState extends State<HomePage> {
             size: 28,
           ),
         ),
-        // Right Icon Button
         actions: [
           IconButton(
             onPressed: () {},
@@ -74,177 +65,10 @@ class _HomePageState extends State<HomePage> {
         ),
       ),
 
-      // Body => SearchField, Containers
-      body: SingleChildScrollView(
-        // Added SingleChildScrollView to prevent layout issues
-        child: Column(
-          children: [
-            //
-            // SearchField
-            //
-            Container(
-              margin: const EdgeInsets.only(top: 40, left: 20, right: 20),
-              decoration: BoxDecoration(
-                color: Colors.transparent,
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.black.withOpacity(0.11),
-                    blurRadius: 60,
-                    spreadRadius: 0.0,
-                  ),
-                ],
-                borderRadius: BorderRadius.circular(20),
-              ),
-              child: const TextField(
-                decoration: InputDecoration(
-                  filled: true,
-                  fillColor: Colors.white,
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.all(Radius.circular(20)),
-                    borderSide: BorderSide.none,
-                  ),
-                  hintText: 'Search Items',
-                  hintStyle: TextStyle(
-                    color: Color.fromARGB(255, 69, 69, 69),
-                  ),
-                  prefixIcon: Icon(Icons.search),
-                ),
-              ),
-            ),
-
-            // Create distance space
-            const SizedBox(
-              height: 20,
-            ),
-
-            // Main image container
-            Container(
-              height: 350,
-              width: 350,
-              decoration: BoxDecoration(
-                image: const DecorationImage(
-                  image: AssetImage('assets/icon/dog.png'),
-                  fit: BoxFit.cover,
-                ),
-                color: Colors.amber,
-                borderRadius: BorderRadius.circular(20),
-              ),
-            ),
-
-            // 2 Rows (left & right)
-            // 2 Rows (left & right)
-            Row(
-              children: [
-                // 1st Column
-                Padding(
-                  padding: const EdgeInsets.only(left: 20, top: 20),
-                  child: Column(
-                    children: [
-                      // #1 box
-                      Container(
-                        height: 160,
-                        width: 160,
-                        decoration: BoxDecoration(
-                          image: const DecorationImage(
-                            image: AssetImage('assets/images/Evee1.jpg'),
-                            fit: BoxFit.cover,
-                          ),
-                          color: Colors.amber,
-                          borderRadius: BorderRadius.circular(20),
-                        ),
-                      ),
-                      const SizedBox(height: 30),
-                      // #2 box
-                      Container(
-                        height: 160,
-                        width: 160,
-                        decoration: BoxDecoration(
-                          image: const DecorationImage(
-                            image: AssetImage('assets/images/Evee2.jpg'),
-                            fit: BoxFit.cover,
-                          ),
-                          color: Colors.amber,
-                          borderRadius: BorderRadius.circular(20),
-                        ),
-                      ),
-                      const SizedBox(height: 30),
-                      // #3 box
-                      Container(
-                        height: 160,
-                        width: 160,
-                        decoration: BoxDecoration(
-                          image: const DecorationImage(
-                            image: AssetImage('assets/images/Evee3.jpg'),
-                            fit: BoxFit.cover,
-                          ),
-                          color: Colors.amber,
-                          borderRadius: BorderRadius.circular(20),
-                        ),
-                      ),
-                      const SizedBox(height: 10),
-                    ],
-                  ),
-                ),
-
-                // 2nd Column
-                Padding(
-                  padding: const EdgeInsets.only(left: 20, top: 20),
-                  child: Column(
-                    children: [
-                      // #4 box
-                      Container(
-                        height: 160,
-                        width: 160,
-                        decoration: BoxDecoration(
-                          image: const DecorationImage(
-                            image: AssetImage('assets/images/Billy1.jpg'),
-                            fit: BoxFit.cover,
-                          ),
-                          color: Colors.amber,
-                          borderRadius: BorderRadius.circular(20),
-                        ),
-                      ),
-                      const SizedBox(height: 30),
-                      // #5 box
-                      Container(
-                        height: 160,
-                        width: 160,
-                        decoration: BoxDecoration(
-                          image: const DecorationImage(
-                            image: AssetImage('assets/images/Billy2.jpg'),
-                            fit: BoxFit.cover,
-                          ),
-                          color: Colors.amber,
-                          borderRadius: BorderRadius.circular(20),
-                        ),
-                      ),
-                      const SizedBox(height: 30),
-                      // #6 box
-                      Container(
-                        height: 160,
-                        width: 160,
-                        decoration: BoxDecoration(
-                          image: const DecorationImage(
-                            image: AssetImage('assets/images/Billy3.jpeg'),
-                            fit: BoxFit.cover,
-                          ),
-                          color: Colors.amber,
-                          borderRadius: BorderRadius.circular(20),
-                        ),
-                      ),
-                      const SizedBox(height: 10),
-                    ],
-                  ),
-                ),
-              ],
-            ),
-          ], // End of column children
-        ),
-      ),
+      // Switch between pages based on _currentIndex
+      body: _pages[_currentIndex],
 
       // Bottom NavBar
-      // Here we call the buildBottomNavBar widget
-      // From the navigation.dart
       bottomNavigationBar: buildBottomNavBar(_currentIndex, _onTap),
     );
   }
